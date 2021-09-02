4af4c3d4a38f5e7133a2cc2abc24913326f18005
darrylmurphy <darrylmurphy@users.noreply.github.com>
2021-09-02 10:53:57 +0100

    Upgrade to play28

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,17 +6,19 @@ import sbt.{ModuleID, _}
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-27"  % "5.8.0",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.8.0",
     "com.typesafe.play"       %% "play-json-joda"             % "2.7.4"
   )
 
   val test = Seq(
-    "org.scalatest"           %% "scalatest"                % "3.0.8"                 % "test",
+    "org.scalatest"           %% "scalatest"                % "3.2.7"                 % "test",
     "com.typesafe.play"       %% "play-test"                % current                 % "test",
-    "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"                 % "test, it",
-    "com.github.tomakehurst"  %  "wiremock-standalone"      % "2.27.2"                % "test, it",
-    "org.mockito"             %  "mockito-all"              % "1.10.19"               % "test, it"
+    "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test",
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"                 % "test",
+    "org.scalatestplus"       %% "scalatestplus-mockito"    % "1.0.0-M2"              % "test",
+    "com.github.tomakehurst"  %  "wiremock-standalone"      % "2.27.2"                % "test",
+    "org.mockito"             %  "mockito-all"              % "1.10.19"               % "test",
+    "com.vladsch.flexmark"    % "flexmark-all"              % "0.35.10"                % "test"
   )
 
   val akkaVersion = "2.6.7"
